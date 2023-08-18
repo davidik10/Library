cb9074943e67ae0e804c28af29b38c8dae246fc0
davidik <davidshpigelman@gmail.com>
2023-08-18 08:35:40 +0300

    Remove the language

--- script.js
+++ script.js
@@ -1,43 +1,8 @@
 let title,author,pagesRead,pagesTotal;
-let language = 'english';
 let bookCard,bookCards,bookIndex;
 let library = [];
 let clicked = false;
 const form =  document.querySelector('form');
-const flags = document.querySelectorAll('.languages img');
-
-//Add event listeners to flag icons
-flags.forEach(flag => flag.addEventListener('click', changeLanguage))
-
-//Change to a right language according to every country flag icon
-function changeLanguage(e) {
-    removeForm(e); // Initialize form
-    const languagesDiv = document.querySelector('.languages');
-    const languageText = document.querySelector('.languages h3');
-    const pageTitle = document.querySelector('h1');
-    let country = e.target.src.split('/')[4].split('.')[0]; // Pick the country string from each flag clicked.
-    if (country === 'united-kingdom') {
-        languagesDiv.style.flexDirection = 'row';
-        languageText.style.direction = 'ltr';
-        language = 'english';
-        languageText.textContent = 'Choose a language:';
-        pageTitle.textContent = 'Library';
-    }
-    else if (country === 'russia') {
-        languagesDiv.style.flexDirection = 'row';
-        languageText.style.direction = 'ltr';
-        language = 'russian';
-        languageText.textContent = 'Выберите язык:';
-        pageTitle.textContent = 'Библиотека';
-    }
-    else if (country === 'israel') {
-        languagesDiv.style.flexDirection = 'row-reverse';
-        languageText.style.direction = 'rtl';
-        language = 'hebrew';
-        languageText.textContent = 'בחר שפה:';
-        pageTitle.textContent = 'ספריה';
-    }
-}
 
 //Creating forms when 'addBook' button is clicked
 const addBook = document.querySelector('.addBook');
@@ -64,17 +29,11 @@ function Book(title,author,pagesRead,pagesTotal) {
         for (let i=0; i<4; i++) {
             const div = document.createElement('div');
             const label = document.createElement('label');
-            if (language === 'hebrew') {
-                div.style.flexDirection = 'row-reverse';
-                label.style.direction = 'rtl';
-            }
             const input = document.createElement('input');
             input.setAttribute('required','');
             if (i === 0) {
                 label.setAttribute('label', 'title');
-                if (language === 'english') label.textContent = 'Book Title:';
-                else if (language === 'russian') label.textContent = 'Название:';
-                else if (language === 'hebrew') label.textContent = 'שם הספר:';
+                label.textContent = 'Book Title:';
                 input.setAttribute('type','text');
                 input.setAttribute('id','title');
                 div.appendChild(label);
@@ -83,9 +42,7 @@ function Book(title,author,pagesRead,pagesTotal) {
             }
             if (i === 1) {
                 label.setAttribute('label', 'author');
-                if (language === 'english') label.textContent = 'Author:';
-                else if (language === 'russian') label.textContent = 'Автор:';
-                else if (language === 'hebrew') label.textContent = 'מחבר:';
+                label.textContent = 'Author:';
                 input.setAttribute('type','text');
                 input.setAttribute('id','author');
                 div.appendChild(label);
@@ -95,9 +52,7 @@ function Book(title,author,pagesRead,pagesTotal) {
             if (i === 2) {
                 input.removeAttribute('required','');
                 label.setAttribute('label', 'from');
-                if (language === 'english') label.textContent = 'Pages Read:';
-                else if (language === 'russian') label.textContent = 'Прочитаны страницы:';
-                else if (language === 'hebrew') label.textContent = 'עמודים שנקראו:'
+                label.textContent = 'Pages Read:';
                 input.setAttribute('type','number');
                 input.setAttribute('id','from');
                 input.setAttribute('min','0');
@@ -110,9 +65,7 @@ function Book(title,author,pagesRead,pagesTotal) {
                 console.log(document.getElementById('from').value)
                 input.removeAttribute('required','');
                 label.setAttribute('label', 'to');
-                if (language === 'english') label.textContent = 'Pages Total:';
-                else if (language === 'russian') label.textContent = 'Всего страниц:';
-                else if (language === 'hebrew') label.textContent = 'סך העמודים:';
+                label.textContent = 'Pages Total:';
                 input.setAttribute('type','number');
                 input.setAttribute('id','to');
                 input.setAttribute('min',`1`);
@@ -123,12 +76,7 @@ function Book(title,author,pagesRead,pagesTotal) {
         }
         const button = document.createElement('button');
         button.setAttribute('type','submit');
-        if (language === 'english') button.textContent = 'Add';
-        else if (language === 'russian') {
-            button.textContent = 'Добавлять';
-            button.style.width = '80px';
-        }
-        else if (language === 'hebrew') button.textContent = 'הוסף';
+        button.textContent = 'Add';
         form.appendChild(button);
         form.addEventListener('submit',(e) => {
             e.preventDefault();
@@ -178,19 +126,11 @@ function booksDisplay(library) {
         remove.classList.add('remove');
         bookIndex = library.indexOf(book) 
         title.textContent = `${book.title}`;
-        if (language === 'hebrew') title.style.direction = 'rtl';
         bookCard.appendChild(title);
-        if (language === 'english') author.textContent = `Author: ${book.author}`;
-        else if (language === 'russian') author.textContent = `Автор: ${book.author}`;
-        else if (language === 'hebrew') {
-            author.textContent = `מחבר: ${book.author}`;
-            author.style.direction = 'rtl';
-        }
+        author.textContent = `Author: ${book.author}`;
         bookCard.appendChild(author);
         pagesDisplay(book);
-        if (language === 'english') remove.textContent = 'Remove';
-        else if (language === 'russian') remove.textContent = 'Удалять';
-        else if (language === 'hebrew') remove.textContent = 'הסר';
+        remove.textContent = 'Remove';
         bookCard.appendChild(remove);
         bookCard.setAttribute('data-index', `${bookIndex}`);
         console.log(bookCard);
@@ -211,9 +151,7 @@ function pagesDisplay(book) {
     pagesToggle.max = book.pagesTotal;
     pagesToggle.addEventListener('change',(e) => {
         if (e.target.value === pagesTotal) {
-            if (language === 'english') pages.textContent = 'Finished';
-            else if (language === 'russian') pages.textContent = 'Прочитана';
-            else if (language === 'hebrew') pages.textContent = 'נקרא';
+            pages.textContent = 'Finished';
             pages.style.color = 'green';
         }
         else {
@@ -221,15 +159,7 @@ function pagesDisplay(book) {
             pagesText.style.color = 'black';
         }
     });
-    if (language === 'english') pages.textContent = 'Pages:';
-    else if (language === 'russian') pages.textContent = 'Страницы:';
-    else if (language === 'hebrew') {
-        pages.textContent = 'עמודים:'
-        pages.style.direction = 'rtl';
-        pages.style.position = 'relative';
-        pages.style.right = '70px'
-
-    } 
+    pages.textContent = 'Pages:';
     // Don't diasplay the pages toggle or text if the user hasn't submit it
         if (pagesTotal !== '') {
             pages.appendChild(pagesToggle);
